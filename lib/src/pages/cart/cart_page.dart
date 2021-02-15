aa4c2375c0850dd88602ffe46ce7d673dd12e6d2
goncalveshigino <goncalveshigono20@gmail.com>
2021-02-15 21:31:22 +0100

    feita

--- lib/src/pages/cart/cart_page.dart
+++ lib/src/pages/cart/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:cumidas/src/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 import 'components/cart_car.dart';
@@ -11,151 +12,131 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.white,
-       body: ListView(
-         padding: EdgeInsets.symmetric(horizontal: 10.0),
-         scrollDirection: Axis.vertical,
-
-         children: <Widget>[
-             CartCar(),
-             CartCar(),
-             CartCar(),
-             CartCar(),
-         ],
-       ),
-
-       bottomNavigationBar: _TotalContainer(),
+      backgroundColor: Colors.white,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          CartCar(),
+          CartCar(),
+          CartCar(),
+        ],
+      ),
+      bottomNavigationBar: _TotalContainer(),
     );
   }
 
-
-   Widget _TotalContainer(){
-        return Container(
-          height: 200.0,
-          padding: EdgeInsets.only(
-            left: 20.0,
-            right: 20.0
-          ),
-           margin: EdgeInsets.only(
-            top: 20.0,
-  
-          ),
-          child: Column(
+  Widget _TotalContainer() {
+    return Container(
+      height: 200.0,
+      padding: EdgeInsets.only(left: 10.0, right: 10.0),
+      margin: EdgeInsets.only(
+        top: 20.0,
+      ),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-
-                  Text(
-                    "Subtotal",
-                    style: TextStyle(
-                      color: Color(0xFF6C6C6D6D),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    "1300,00",
-                    style: TextStyle(
-                      color: Color(0xFF6C6C6D6D),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
+              Text(
+                "Subtotal",
+                style: TextStyle(
+                    color: Color(0xFF6C6C6D6D),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
               ),
-
-              SizedBox(
-                height: 30.0,
-              ),
-
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Taxa de Entrega",
-                    style: TextStyle(
-                      color: Color(0xFF9BA7C6),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    "500,0",
-                    style: TextStyle(
-                      color: Color(0xFF9BA7C6),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
-              ),
-            
-          Divider(height: 20.0,),
-       
-              SizedBox(
-                height: 20.0,
-              ),
-               Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Total",
-                    style: TextStyle(
-                      color: Color(0xFF9BA7C6),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    "11500,00",
-                    style: TextStyle(
-                      color: Color(0xFF9BA7C6),
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
-              ),
-              
-              SizedBox(
-                height: 10.0,
-              ),
-
-              GestureDetector(
-                onTap: (){
-                 
-
-                 //Chamar Login
-                },
-
-                child: Container(
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10.0)
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Finalizar Pagamento",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-
-
+              Text(
+                "1300,00",
+                style: TextStyle(
+                    color: Color(0xFF6C6C6D6D),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              )
             ],
           ),
-        );
+          SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Taxa de Entrega",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "500,0",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Divider(
+            height: 2.0,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Total",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "11500,00",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          GestureDetector(
+            onTap: () {
+              //Chamar Login
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => LoginPage()));
+            },
+            child: Container(
+              height: 55.0,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Center(
+                child: Text(
+                  "Finalizar Pagamento",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
