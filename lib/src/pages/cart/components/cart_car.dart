8b48bc1f93cd81012d5eded3639f9ae686c05e1c
goncalveshigino <goncalveshigono20@gmail.com>
2021-02-15 23:14:08 +0100

    tela cadastro

--- lib/src/pages/cart/components/cart_car.dart
+++ lib/src/pages/cart/components/cart_car.dart
@@ -9,123 +9,123 @@ class _CartCarState extends State<CartCar> {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Padding(
-          padding: EdgeInsets.symmetric( horizontal: 18.0, vertical: 10.0),
-             child: Row(
-                 mainAxisSize: MainAxisSize.max,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                 children: <Widget>[
-                   Container(
-                     decoration: BoxDecoration(
-                         border: Border.all(color: Color(0xFFD3D3D3), width: 2.0),
-                         borderRadius: BorderRadius.circular(15.0)
-                     ),
-
-                     width: 45.0,
-                     height: 73.0,
-                     child: Padding(
-                       padding: EdgeInsets.symmetric(
-                         horizontal: 10.0,
-                       ),
-                       child: Column(
-                         children: <Widget>[
-                            InkWell(
-                              onTap: (){},
-                              child: Icon(Icons.keyboard_arrow_up,
-                              color: Color(0xFFD3D3D3))),
-                              Text(
-                                "0",
-                                style: TextStyle(fontSize: 18.0, color: Colors.grey),
-                              ),
-                            InkWell(
-                              onTap: (){},
-                              child: Icon(Icons.keyboard_arrow_down,color: Color(0xFFD3D3D3))),
-                         ],
-                       ),
-                     ),
-                   ),
-
-                   SizedBox(
-                      width: 10.0,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 10.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFD3D3D3), width: 2.0),
+                  borderRadius: BorderRadius.circular(15.0)),
+              width: 45.0,
+              height: 73.0,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    InkWell(
+                        onTap: () {},
+                        child: Icon(Icons.keyboard_arrow_up,
+                            color: Color(0xFFD3D3D3))),
+                    Text(
+                      "0",
+                      style: TextStyle(fontSize: 18.0, color: Colors.grey),
                     ),
-
-                    Container(
-                      height: 80.0,
-                      width: 80.0,
-                      decoration: BoxDecoration(
-                         image: DecorationImage(
-                           image: AssetImage("assets/images/01.png"),
-                           fit: BoxFit.cover
-                         ),
-                        borderRadius: BorderRadius.circular(35.0),
-                      ),
-                    ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    InkWell(
+                        onTap: () {},
+                        child: Icon(Icons.keyboard_arrow_down,
+                            color: Color(0xFFD3D3D3))),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            Container(
+              height: 80.0,
+              width: 80.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/01.png"),
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(35.0),
+              ),
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "X-Tudão",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "R\u{0024} 10,00",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Container(
+                  height: 25.0,
+                  width: 120.0,
+                  child: ListView(
                     children: <Widget>[
-                        Text(
-                          "X-Tudão",
-                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox( height: 5.0,),
-                        Text(
-                          "10,00",
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "Sanduiches",
+                            style: TextStyle(
+                                color: Color(0xFFD3D3D3),
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                       SizedBox( height: 5.0,),
-                       Container(
-                         height: 25.0,
-                         width: 120.0,
-                         child: ListView(
-                             children: <Widget>[
-                               Row(
-                                  children: <Widget>[
-                                    Text(
-                                      "Sanduiches",
-                                      style: TextStyle(
-                                        color: Color(0xFFD3D3D3),
-                                        fontWeight: FontWeight.bold),),
-
-                                      SizedBox( 
-                                        width: 5.0,
-                                      ),
-                                      InkWell(
-                                        onTap: (){},
-                                        child: Text(
-                                          "X",
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.red
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox( width: 10.0,),
-                                  ],
-                                ),
-                             ],
-                         ),
-                       )
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              "X",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: (){},
-                    child: Icon(Icons.cancel,
-                    color: Colors.grey,
-                    ),
-                  )
-                 ],
-             ),
+                )
+              ],
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {},
+              child: Icon(
+                Icons.cancel,
+                color: Colors.grey,
+              ),
+            )
+          ],
         ),
+      ),
     );
   }
 }
